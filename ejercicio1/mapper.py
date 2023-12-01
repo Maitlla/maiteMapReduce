46b0da1dba45da537bc2aa0113466ae7bb58e99c
Maitlla <maitlla@gmail.com>
2023-12-01 21:26:14 +0100

    primer_commit

---
+++ ejercicio1/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+
+for line in sys.stdin:
+    if line.count("\t")==5:
+        data = line.strip().split("\t")
+        date, time, store, item, cost, payment = data
+        print(store+"\t"+cost)
+
+
