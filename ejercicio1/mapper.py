c88156cf8a8e5721bd174913e7be3dfb3297f267
Maitlla <maitlla@gmail.com>
2023-12-10 01:52:13 +0100

    ejercicio1

--- ejercicio1/mapper.py
+++ ejercicio1/mapper.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
+
 import sys
 
 for line in sys.stdin:
-    if line.count("\t")==5:
+    data = line.strip().split("\t")
+    # Comprobamos si la línea tiene el formato correcto
+    if len(data) != 6: 
+        continue
+    date, time, store, item, cost, payment = data
+    print(f"{store}\t{cost}")
+
+
+"""
+# comprobar el código
+with open("mini-purchase.txt", "r") as file:
+    for line in file:
         data = line.strip().split("\t")
+        if len(data) != 6:
+            print(f"Error: La línea no tiene suficientes campos: {data}")
+            continue
         date, time, store, item, cost, payment = data
-        print(store+"\t"+cost)
+        print(f"{store}\t{cost}")
 
-
+"""
